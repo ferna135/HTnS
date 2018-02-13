1612a478159a71be62e53770a415048abf49263f
ferna135 <ferna135@uni.coventry.ac.uk>
2018-02-13 21:52:44 +0000

    Added project to github :3

---
+++ Assets/Scripts/GravityGun.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGun : MonoBehaviour {
+    //Code based on PushyPixels video
+    //Link: https://www.youtube.com/watch?v=sxjJTNDmsug
+
+
+    public float grabDistance = 10f;
+    public string grabButton = "Fire1";
+    public Transform holdPosition;
+    public LayerMask layerMask = -1;
+    public bool defaultObjectState = false;
+
+    public Texture2D crossHairImage;
+
+
+    public float smooth = 5f;
+    private Quaternion targetRotation;
+
+
+    private GameObject heldObject = null; 
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crossHairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crossHairImage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crossHairImage.width, crossHairImage.height), crossHairImage);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector2 middleOfScreen = new Vector2(Screen.width / 2, Screen.height / 2);
+        Ray myRay = Camera.main.ScreenPointToRay(middleOfScreen);
+
+        if (heldObject == null)
+        {
+            if (Input.GetButton(grabButton))
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(myRay, out hit, grabDistance, layerMask))
+                {
+                    heldObject = hit.collider.gameObject;
+                    defaultObjectState = heldObject.GetComponent<Rigidbody>().isKinematic;
+                    heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+
+
+            }
+
+        }else
+        //Object is grabbed
+        {
+            //Rotate to the left
+            if (Input.GetKey(KeyCode.Q))
+            {
+                RotateObject(heldObject, "l");
+            }
+
+            //Rotate to the right
+            if (Input.GetKey(KeyCode.E))
+            {
+                RotateObject(heldObject, "r");
+            }
+
+
+
+            //Let go of the object
+            if (Input.GetButtonUp(grabButton))
+            {
+                heldObject.GetComponent<Rigidbody>().isKinematic = defaultObjectState;
+                heldObject = null;
+            }
+            else
+            {
+                heldObject.transform.position = holdPosition.position;
+            }
+            
+        }
+
+	}
+
+
+
+    void RotateObject(GameObject obj, string orientation) {
+
+
+        targetRotation = obj.transform.rotation;
+
+        if (orientation == "l")
+        {
+            targetRotation *= Quaternion.AngleAxis(-10, Vector3.forward);
+            obj.transform.rotation = Quaternion.Lerp(obj.transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+        }
+
+        if (orientation == "r")
+        {
+            targetRotation *= Quaternion.AngleAxis(10, Vector3.forward);
+            obj.transform.rotation = Quaternion.Lerp(obj.transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+        }
+
+    }
+
+    
+
+}
---
+++ Assets/Scripts/puzzle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzle : MonoBehaviour {
+    public GameObject cube;
+    private GameObject[] cubes;
+    public int gameDif = 5;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        for (int y = -7; y < 0; y++)
+        {
+            
+            
+            for (int x = 20; x < 27; x++)
+            {
+                Vector3 spawnPos = new Vector3(x, y, 10f);
+                Instantiate(cube, spawnPos, Quaternion.identity);
+            }
+        }
+
+        cubes = GameObject.FindGameObjectsWithTag("cube");
+
+        Debug.Log(cubes.Length);
+        CubeMooving();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //impCube.transform.Rotate(0, 100 * Time.deltaTime, 0);
+        
+        
+        
+    }
+    void CubeMooving()
+    {
+        
+        for (int i = 0; i < gameDif; i++)
+        {
+            int cubsNumber = Random.Range(0, cubes.Length);
+            cubes[cubsNumber].transform.Translate(new Vector3(-10f, 0, 0));
+            cubes[cubsNumber].layer = 9;
+            cubes = GameObject.FindGameObjectsWithTag("cube");
+        }
+        
+        
+
+
+    }
+
+}
