e4fa6a8b35450c73352d99cb5270a4cb2a040997
RwAlastair <alastairv@outlook.com>
2018-02-18 14:26:25 +0000

    UI Visor script added

---
+++ Assets/VisorBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisorBehaviour : MonoBehaviour {
+
+	public Transform canvas;
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+			} else {
+				canvas.gameObject.SetActive (false);
+			}
+		}
+	}
+}
